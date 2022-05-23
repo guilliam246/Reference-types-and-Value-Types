60a0815e480795ab91f02de70465a3436d5f7132
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-23 15:06:23 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reference_types_and_Value_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;
+            Console.WriteLine(String.Format("a: {0}, b: {1}", a, b));
+        }
+    }
+}
