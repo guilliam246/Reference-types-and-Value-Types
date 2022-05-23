a81f45f7d27f0d856b4f3e602e5fabb6b2dea1df
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-23 15:32:22 +1000

    Difference with ref and value using methods

--- Program.cs
+++ Program.cs
@@ -2,8 +2,23 @@
 
 namespace Reference_types_and_Value_Types
 {
-    internal class Program
+
+    //Second part of the lesson below including the class and methods made below
+    public class Person
     {
+        public int Age;
+    }
+    public class Program
+    {
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
         static void Main(string[] args)
         {
             var a = 10;
@@ -15,6 +30,18 @@ namespace Reference_types_and_Value_Types
             var array2 = array1;
             array2[0] = 0;
             Console.WriteLine(String.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+
+            //Second part of the lesson below including the class and methods made above
+
+            var number = 1;
+            Increment(number); // Number will still be one since its in the main method and seen as different from the number in the Increment method
+            Console.WriteLine(number);
+
+            var person = new Person() { Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age); // reference type so age will increase
+
+         
         }
     }
 }
