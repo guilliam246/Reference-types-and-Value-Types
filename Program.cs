e043b1a3d539b091c854fb4b79e5e63d45cd4adb
Guilliam Oberholster <guilliamoberholster@gmail.com>
2022-05-23 15:13:39 +1000

    Showing the difference between ref and value and how the values will change

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace Reference_types_and_Value_Types
             var b = a;
             b++;
             Console.WriteLine(String.Format("a: {0}, b: {1}", a, b));
+
+            var array1 = new int[3] {1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(String.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
         }
     }
 }
